b3a1fcd3ef71a2615735aaa73ce321721274f1ae
Jamie Vullo <jvullo@protonmail.com>
2019-07-15 13:13:31 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,10 +1,12 @@
-class Dog
-  def name=(dogs_name)
-    @this_dogs_name = dogs_name
+class Dog 
+  
+  def name=(name)
+    @this_dogs_name = name
   end
   
   def name 
     @this_dogs_name
   end
-    
+  
+  
 end
